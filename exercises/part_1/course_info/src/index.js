4ecbeb41c9cc22da88c9348bcaf7bf943f953016
Zenghao <zenghao.wang@mail.utoronto.ca>
2020-04-06 23:15:42 -0400

    Exercise 1.5: Refactored course into one object

--- exercises/part_1/course_info/src/index.js
+++ exercises/part_1/course_info/src/index.js
@@ -4,10 +4,11 @@ import ReactDOM from "react-dom";
 const Header = (props) => {
   return (
     <div>
-      <h1>{props.course}</h1>
+      <h1>{props.courseName}</h1>
     </div>
   );
 };
+
 const Part = (props) => {
   return (
     <div>
@@ -15,6 +16,7 @@ const Part = (props) => {
     </div>
   );
 };
+
 const Content = (props) => {
   return (
     <div>
@@ -48,18 +50,20 @@ const Total = (props) => {
 };
 
 const App = () => {
-  const course = "Half Stack application development";
-  const parts = [
-    { name: "Fundamentals of React", exercises: 10 },
-    { name: "Using props to pass data", exercises: 7 },
-    { name: "State of a component", exercises: 14 },
-  ];
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      { name: "Fundamentals of React", exercises: 10 },
+      { name: "Using props to pass data", exercises: 7 },
+      { name: "State of a component", exercises: 14 },
+    ],
+  };
 
   return (
     <div>
-      <Header course={course}></Header>
-      <Content parts={parts}></Content>
-      <Total parts={parts}></Total>
+      <Header courseName={course.name}></Header>
+      <Content parts={course.parts}></Content>
+      <Total parts={course.parts}></Total>
     </div>
   );
 };
