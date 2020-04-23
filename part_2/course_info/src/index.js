0c58572c7639796281841d695527c95e9566c9f0
Zenghao Wang <zenghao.wang@mail.utoronto.ca>
2020-04-23 17:49:15 -0400

    Exercise 2.4

--- part_2/course_info/src/index.js
+++ part_2/course_info/src/index.js
@@ -34,31 +34,52 @@ const Total = ({ total }) => (
 );
 
 const App = () => {
-    const course = {
-        id: 1,
-        name: "Half Stack application development",
-        parts: [
-            {
-                name: "Fundamentals of React",
-                exercises: 10,
-                id: 1,
-            },
-            {
-                name: "Using props to pass data",
-                exercises: 7,
-                id: 2,
-            },
-            {
-                name: "State of a component",
-                exercises: 14,
-                id: 3,
-            },
-        ],
-    };
+    const courses = [
+        {
+            id: 1,
+            name: "Half Stack application development",
+            parts: [
+                {
+                    name: "Fundamentals of React",
+                    exercises: 10,
+                    id: 1,
+                },
+                {
+                    name: "Using props to pass data",
+                    exercises: 7,
+                    id: 2,
+                },
+                {
+                    name: "State of a component",
+                    exercises: 14,
+                    id: 3,
+                },
+            ],
+        },
+
+        {
+            name: "Node.js",
+            id: 2,
+            parts: [
+                {
+                    name: "Routing",
+                    exercises: 3,
+                    id: 1,
+                },
+                {
+                    name: "Middlewares",
+                    exercises: 7,
+                    id: 2,
+                },
+            ],
+        },
+    ];
 
     return (
         <div>
-            <Course course={course} />
+            {courses.map((course) => (
+                <Course course={course} />
+            ))}
         </div>
     );
 };
