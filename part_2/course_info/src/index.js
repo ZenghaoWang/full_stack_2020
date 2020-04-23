a614dbf101e050f36fb841731c63e4e355e95f84
Zenghao Wang <zenghao.wang@mail.utoronto.ca>
2020-04-23 17:56:47 -0400

    Exercise 2.5

--- part_2/course_info/src/index.js
+++ part_2/course_info/src/index.js
@@ -1,37 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-const Course = ({ course }) => {
-    return (
-        <div>
-            <Header header={course.name}></Header>
-            <Content partArr={course.parts}></Content>
-        </div>
-    );
-};
-
-const Header = ({ header }) => <h1>{header}</h1>;
-
-const Content = ({ partArr }) => {
-    const total = partArr.reduce((a, b) => a + b.exercises, 0);
-
-    return (
-        <div>
-            {partArr.map((part) => (
-                <Part part={part.name} numExercises={part.exercises}></Part>
-            ))}
-            <Total total={total}></Total>
-        </div>
-    );
-};
-
-const Part = ({ part, numExercises }) => <p>{`${part} ${numExercises}`}</p>;
-
-const Total = ({ total }) => (
-    <p>
-        <b>{`Total Number of Exercises: ${total}`}</b>
-    </p>
-);
+import Course from "./components/Course";
 
 const App = () => {
     const courses = [
