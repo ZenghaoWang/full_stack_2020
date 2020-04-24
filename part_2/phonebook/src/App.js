08ce06e6a96ffbfe2557fcfd075e070eaa7f0bc9
Zenghao Wang <zenghao.wang@mail.utoronto.ca>
2020-04-23 20:39:59 -0400

    Exercise 2.6

---
+++ part_2/phonebook/src/App.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import Person from "./components/Person";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const addName = (event) => {
+    event.preventDefault();
+
+    const personObject = {
+      name: newName,
+    };
+
+    setPersons(persons.concat(personObject));
+    console.log(persons);
+
+    setNewName("");
+  };
+
+  // Updates everytime the user types a character
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map((person) => (
+          <Person key={person.name} person={person}></Person>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
